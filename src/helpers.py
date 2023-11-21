1c6f27a0871faf0599060861c56e093b6afc3e16
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-11-20 21:35:08 -0500

    Fix bug in create_splits

--- src/helpers.py
+++ src/helpers.py
@@ -8,10 +8,16 @@ import numpy as np
 import spacy
 from spacy.tokens import Doc
 from spacy.language import Language
+from sklearn.model_selection import train_test_split
 
-from src.globals import DATA_FPATH, SANCHEZ_FAMILY, ENABLED_SPACY_COMPONENTS
-from src.configured_experiment import ConfiguredExperiment
-from src.experiment import Experiment
+from src.globals import (
+    DATA_FPATH,
+    SANCHEZ_FAMILY_LABELS,
+    ENABLED_SPACY_COMPONENTS,
+    SPLIT_RATIOS,
+)
+from src.schema.configured_experiment import ConfiguredExperiment
+from src.schema.experiment import Experiment
 from src.config import FEATURE_EXTRACTORS
 
 
@@ -24,14 +30,15 @@ def load_data() -> Tuple["pd.Series[str]", "pd.Series[str]"]:
             speakers respectively as Series.
     """
     df = pd.read_csv(DATA_FPATH)
-    df = df[df["speaker"].isin(SANCHEZ_FAMILY)]
-    X, y = df["utterance"], df["speaker"]
+    df = df[df["name"].isin(SANCHEZ_FAMILY_LABELS)]
+    X, y = df["line"], df["name"]
     return X, y
 
 
 def create_splits(index: pd.Index) -> "pd.Series[Literal['train', 'dev', 'test']]":
     """
-    Randomly assigns 'train', 'dev', or 'test' to each element of a pandas Series index.
+    Randomly assigns 'train', 'dev', or 'test' to each element of a pandas Series index
+    according to the partition ratios defined in `SPLIT_RATIOS`.
 
     Args:
         index (pd.Index): The index of a pandas Series.
@@ -39,13 +46,26 @@ def create_splits(index: pd.Index) -> "pd.Series[Literal['train', 'dev', 'test']
     Returns:
         pd.Series: A Series with the same index, with labels 'train', 'dev', or 'test'.
     """
-    labels = np.random.choice(["train", "dev", "test"], size=len(index))
-    return pd.Series(labels, index=index)
+    # Split between train and the rest
+    train_index, rest_index = train_test_split(
+        index, test_size=SPLIT_RATIOS["test"] + SPLIT_RATIOS["dev"], shuffle=True
+    )
+    # Adjust dev ratio to account for the fact that we already split off test
+    ratio = SPLIT_RATIOS["dev"] / (SPLIT_RATIOS["dev"] + SPLIT_RATIOS["test"])
+    # Second split between test and dev
+    test_index, dev_index = train_test_split(rest_index, test_size=ratio, shuffle=True)
+
+    # Create the final Series with labels
+    labels = pd.Series(index=index, dtype="object")
+    labels[train_index] = "train"
+    labels[test_index] = "test"
+    labels[dev_index] = "dev"
+    return labels
 
 
 def preprocess_spacy_doc(
-    nlp: Language,
     input_text: str,
+    nlp: Language,
 ) -> Doc:
     """Applies SpaCy preprocessing to the input text and returns the processed Doc.
 
@@ -76,7 +96,11 @@ def initialize_experiments(
         List[Experiment]: A list of initialized experiment objects.
     """
     utterances, labels = load_data()
-    train_dev_test_split = create_splits(utterances.index)
+    train_test_dev = create_splits(utterances.index)
+    # Print number of uterrances per speaker per split
+    for split in ["train", "dev", "test"]:
+        print(f"Number of utterances per speaker in {split} split:")
+        print(labels[train_test_dev == split].value_counts())
     # Organize experiments by SpaCy model
     configured_experiments_by_spacy_model = defaultdict(list)
     for configured_experiment in configured_experiments:
@@ -107,12 +131,12 @@ def initialize_experiments(
         # Initialize experiment objects
         for configured_experiment in configured_experiments:
             initialized_experiments.append(
-                Experiment(
-                    model_type=configured_experiment.Experiment,
-                    feature_extractors=caching_extractors,
+                configured_experiment.Experiment(
                     docs=docs,
                     labels=labels,
-                    train_dev_test_split=train_dev_test_split,
+                    train_test_dev=train_test_dev,
+                    spacy_model_name=spacy_model_name,
+                    feature_extractors=caching_extractors,
                     model_load_time=model_load_time,
                     avg_utterance_preprocessing_time=avg_utterance_preprocessing_time,
                 )
